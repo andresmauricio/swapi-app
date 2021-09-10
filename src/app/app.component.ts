5143420a384df0f0cadf54f288133d2283e22b19
andresacelas <andresacelas@outlook.com>
2021-09-09 22:57:59 -0500

    render categories api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwapiService } from './core/swapi.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'swapi-app';
+  list: any = []
+  constructor(private swapiService: SwapiService) {
+
+  }
+  ngOnInit(): void {
+    this.swapiService.getRoot().subscribe((response) => {
+      const list = [];
+      for(const key in response) {
+        list.push({name: key, api: response[key]})        
+      }
+      this.list = [...list];
+    })
+  }
 }
