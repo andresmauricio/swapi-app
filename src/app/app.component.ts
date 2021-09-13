0f5f0f90f8ecd8b5cb8ffff863fdd9495ef083d3
andresacelas <andresacelas@outlook.com>
2021-09-12 22:20:36 -0500

    get categoires in container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,9 @@ import { SwapiService } from './core/swapi.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'swapi-app';
-  list: any = []
-  constructor(private swapiService: SwapiService) {
-
-  }
-  ngOnInit(): void {
-    this.swapiService.getRoot().subscribe((response) => {
-      const list = [];
-      for(const key in response) {
-        list.push({name: key, api: response[key]})        
-      }
-      this.list = [...list];
-    })
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
--- src/app/home/containers/home/home.component.ts
+++ src/app/home/containers/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { SwapiService } from 'src/app/core/swapi.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public list: any;
 
-  constructor() { }
+  constructor(private swapiService: SwapiService) {}
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  getCategories() {
+    this.swapiService.getRoot().subscribe((response) => {
+      const list = [];
+      for (const key in response) {
+        list.push({ name: key, api: response[key] });
+      }
+      this.list = [...list];
+    });
+  }
 }
