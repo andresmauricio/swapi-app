555dc912c5a67144827dbf6635081c6fe21964df
andresacelas <andresacelas@outlook.com>
2021-09-13 11:07:08 -0500

    container list of people

--- src/app/people/people-routing.module.ts
+++ src/app/people/people-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListOfPeopleComponent } from './containers/list-of-people/list-of-people.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListOfPeopleComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PeopleRoutingModule { }
+export class PeopleRoutingModule {}
--- src/app/people/people.module.ts
+++ src/app/people/people.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PeopleRoutingModule } from './people-routing.module';
+import { ListOfPeopleComponent } from './containers/list-of-people/list-of-people.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListOfPeopleComponent
+  ],
   imports: [
     CommonModule,
     PeopleRoutingModule
