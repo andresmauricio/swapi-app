b611df50685e0887e3864057a85689658c89b616
andresacelas <andresacelas@outlook.com>
2021-09-13 10:57:04 -0500

    implements peolple module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: 'people',
+    loadChildren: () => import('./people/people.module').then((m) => m.PeopleModule),
+  },
 ];
 
 @NgModule({
