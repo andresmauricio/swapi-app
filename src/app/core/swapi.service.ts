7e888805a125df561bc2fc5db4c5f0fe4a5d088b
andresacelas <andresacelas@outlook.com>
2021-09-13 11:17:25 -0500

    get information of people

--- src/app/core/swapi.service.ts
+++ src/app/core/swapi.service.ts
@@ -1,17 +1,30 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { environment } from "src/environments/environment";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-@Injectable({providedIn: 'root'})
+export enum Categorie {
+  FILMS = 'films',
+  PEOPLE = 'people',
+  PLANETS = 'planets',
+  SPECIES = 'species',
+  STARSSHIPS = 'starships',
+  VEHICLES = 'vehicles',
+}
+
+@Injectable({ providedIn: 'root' })
 export class SwapiService {
   private readonly BASE_API: string;
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
     this.BASE_API = environment.api;
   }
 
   getRoot(): Observable<any> {
     return this.http.get(this.BASE_API);
+  }
+
+  getInformationOfCategorie(categorie: Categorie): Observable<any> {
+    return this.http.get(`${this.BASE_API}${categorie}`);
   }
 }
--- src/app/people/containers/list-of-people/list-of-people.component.ts
+++ src/app/people/containers/list-of-people/list-of-people.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Categorie, SwapiService } from 'src/app/core/swapi.service';
+import { ResponseApi } from '../../models/IPeople';
 
 @Component({
   selector: 'app-list-of-people',
   templateUrl: './list-of-people.component.html',
-  styleUrls: ['./list-of-people.component.scss']
+  styleUrls: ['./list-of-people.component.scss'],
 })
 export class ListOfPeopleComponent implements OnInit {
+  private categorie = Categorie;
+  people!: ResponseApi;
 
-  constructor() { }
+  constructor(private swapiService: SwapiService) {}
 
   ngOnInit(): void {
+    this.swapiService
+      .getInformationOfCategorie(Categorie.PEOPLE)
+      .subscribe((response: ResponseApi) => {
+        console.log(response);
+        this.people = response;
+      });
   }
-
 }
