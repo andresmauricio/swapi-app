62f3672de69082cce1137964d4a3549ca649f3f1
andresacelas <andresacelas@outlook.com>
2021-09-12 22:18:13 -0500

    card component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './containers/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MaterialModule
   ]
 })
 export class HomeModule { }
