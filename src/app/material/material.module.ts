e22b4ac37867d843041d207f26a2bfc885a7fb7d
andresacelas <andresacelas@outlook.com>
2021-09-15 19:06:51 -0500

    show properties people

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatTableModule],
-  exports: [MatCardModule, MatButtonModule, MatTableModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+    MatListModule,
+  ],
+  exports: [MatCardModule, MatButtonModule, MatTableModule, MatListModule],
 })
 export class MaterialModule {}
